45c5d9c39198a815d164b76c689d70d4175a566f
guenchi <louis.deguenchy@gmail.com>
2018-08-26 23:54:30 +0200

    Update base64.c

--- src/base64.c
+++ src/base64.c
@@ -1,6 +1,10 @@
 /* This is a public domain base64 implementation written by WEI Zhicheng. */
 
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 enum {BASE64_INVALID = 0, BASE64_OK};
 
@@ -135,3 +139,18 @@ int base64_decode(const char *in, unsigned int inlen, char *out)
 
 	return len;
 }
+
+int
+main(void)
+{
+	unsigned char input[] = "pleasure.";
+	unsigned char decode[13];
+	char output[13];
+	uint len_decode = 0;
+	bzero(output, sizeof(output));
+	base64_encode(input, sizeof(input) - 1, output);
+	len_decode = base64_decode(output, sizeof(output) - 1, decode);
+
+	return 0;
+}
+
